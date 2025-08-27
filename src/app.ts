3ec7aec77a5c7ea56fabe080b1480372d3f83900
imran007-Programming <imranphero@gmail.com>
2025-08-27 15:53:23 +0600

    change_app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,25 @@
 import express, { Request, Response } from "express";
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { Allrouters } from "./app/Allroutes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { envVars } from "./app/config/env";
 
 const app = express();
 app.use(express.json());
+app.set("trust proxy", 1);
 /* cookieparser */
 app.use(cookieParser());
 
 app.use(
-  cors(
-    {
-    origin:envVars.FRONTEND_URL ,
-    credentials: true, 
-  }
-)
+  cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true,
+  })
 );
 
-
 /* base url */
-app.use("/api/v1",Allrouters)
-
+app.use("/api/v1", Allrouters);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({
@@ -31,6 +28,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 // /* global error handler */
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
--- src/app/utils/setCookies.ts
+++ src/app/utils/setCookies.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { envVars } from "../config/env";
 
 export interface AuthToken {
   accessToken?: string;
@@ -6,21 +7,18 @@ export interface AuthToken {
 }
 
 export const setAuthCookies = (res: Response, tokenInfo: AuthToken) => {
-
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: true,
+      secure: envVars.NODE_ENV === "production",
       sameSite: "none",
-     
     });
   }
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
-      secure: true,
+      secure: envVars.NODE_ENV === "production",
       sameSite: "none",
-      
     });
   }
 };
